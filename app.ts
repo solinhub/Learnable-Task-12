d93054033298646f57aeb12e0ecd4da401e7ba18
Solin <solinda90@gmail.com>
2024-04-02 14:57:40 +0100

    Updated Typscript

--- app.ts
+++ app.ts
@@ -69,7 +69,7 @@ app.patch('/api/v1/rooms/:id', authenticateToken, updateRoom);
 app.delete('/api/v1/rooms/:id', authenticateToken, deleteRoom);
 
 // Define PORT
-const PORT: number | string = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
